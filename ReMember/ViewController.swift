d35a51494a57d226d03709d1e42b29ea02b693c7
Takarkiz <ponpontanuki16@gmail.com>
2019-06-15 16:00:08 +0900

    first commit

---
+++ ReMember/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReMember
+//
+//  Created by 澤田昂明 on 2019/06/15.
+//  Copyright © 2019 澤田昂明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
